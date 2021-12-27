4d6666c0fe31694f8d0af99b3f4e4bea408fed4a
Tomoki_Kaneko <Tomoki_Kaneko@T-MacBook-Pro.local>
2021-12-27 09:51:25 +0900

    Initial Commit

---
+++ app/utils/session.server.ts
@@ -0,0 +1,25 @@
+import { db } from "./db.server";
+import bcrypt from "bcrypt";
+
+type LoginForm = {
+  username: string;
+  password: string;
+};
+
+export async function login({
+  username,
+  password
+}: LoginForm) {
+  const user = await db.user.findUnique({
+    where: { username }
+  });
+  if (!user) return null;
+
+  const isCorrectPassword = await bcrypt.compare(
+    password,
+    user.passwordHash
+  );
+  if (!isCorrectPassword) return null;
+
+  return user;
+}
